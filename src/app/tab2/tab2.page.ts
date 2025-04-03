fa8e0e08d3e388a375755bf4e260c584e626de40
Erick Ortiz <eo9977572@gmail.com>
2025-04-03 12:09:48 -0600

    Update tab2.page.ts

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,28 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  standalone:false
+  standalone: false
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page implements OnInit, OnDestroy {
   lecturas: any[] = [];
   apiUrl: string = 'https://joyful-radiance-production.up.railway.app/lectura/obtenerLecturas';
+  intervalId: any; // Variable para almacenar el ID del intervalo
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.obtenerLecturas();
+
+    // Configurar un intervalo para actualizar las lecturas cada 5 segundos
+    this.intervalId = setInterval(() => {
+      this.obtenerLecturas();
+    }, 5000);
+  }
+
+  ngOnDestroy() {
+    // Limpiar el intervalo cuando se destruya el componente
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
   }
 
   obtenerLecturas() {
     this.http.get<any[]>(this.apiUrl).subscribe(
       (data) => {
         console.log(data);
-        // Filtramos solo las lecturas de "Luvianos"
-        this.lecturas = data.filter(lectura => lectura.lugarEstacion === "Luvianos" && lectura.id === 9);  
+        // Filtrar solo las lecturas con id 8
+        this.lecturas = data.filter(lectura => lectura.id === 8);
       },
       (error) => {
         console.error('Error al obtener lecturas:', error);
@@ -34,4 +47,3 @@ export class Tab2Page implements OnInit {
     return new Date(fecha).toLocaleString();
   }
 }
-
